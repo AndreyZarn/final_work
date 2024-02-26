8ed84f94eec7878102e746ae43443c70697564d3
AndreyZarn <114751502+AndreyZarn@users.noreply.github.com>
2024-02-26 11:23:00 +0300

    Add files via upload

---
+++ configure.py
@@ -0,0 +1,8 @@
+config = {
+    'name': 'shop_bot',
+    'token': '6341204283:AAGDrV-yP7xXx3DHTddH3enAg9kW4E_Myv0',
+    'tokenqiwi': '15f081a31bd21966d6c6b3a19d7429cc',
+    'phoneqiwi': '+79969588096',
+    'telegramid': 1859111689,
+
+}
