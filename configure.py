d50949d51cae7ffc9fce82f092404889f4e87271
AndreyZarn <114751502+AndreyZarn@users.noreply.github.com>
2024-02-26 12:13:26 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,8 +1,8 @@
 config = {
-    'name': 'shop_bot',
-    'token': '6341204283:AAGDrV-yP7xXx3DHTddH3enAg9kW4E_Myv0',
-    'tokenqiwi': '15f081a31bd21966d6c6b3a19d7429cc',
-    'phoneqiwi': '+79969588096',
-    'telegramid': 1859111689,
+    'name': 'название бота',
+    'token': 'токен телеграм бота',
+    'tokenqiwi': 'QIWI токен',
+    'phoneqiwi': 'номер телефона QIWI',
+    'telegramid': ваш телеграм ID,
 
 }
